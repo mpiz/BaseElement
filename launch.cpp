92f1a1379cfaee85e20ed6f866665795b13adfe7
Maxim <elnadrius@gmail.com>
2015-12-22 19:29:49 +0600

    Функции формы семиугольника

--- launch.cpp
+++ launch.cpp
@@ -13,22 +13,25 @@ int main() {
 	nodes.push_back(node(0, 0.8, 0, 2));
 
 	nodes.push_back(node(0.2, 1, 0, 3));
-	nodes.push_back(node(0.8, 1, 0, 4));
 
-	nodes.push_back(node(1, 0.8, 0, 5));
-	nodes.push_back(node(1, 0.2, 0, 6));
+	nodes.push_back(node(0.6, 0.6, 0, 4));
 
-	nodes.push_back(node(0.8, 0, 0, 7));
+	nodes.push_back(node(0.8, 1, 0, 5));
+
+	nodes.push_back(node(1, 0.8, 0, 6));
+	nodes.push_back(node(1, 0.2, 0, 7));
+
+	nodes.push_back(node(0.8, 0, 0, 8));
 
 	VirtualFace_Hrot face(nodes, 1, 0);
 
-	face.input_mesh("Face.dat");
-	face.input_bound("FaceBound.dat");
+	face.input_mesh("Face2.dat");
+	face.input_bound("FaceBound2.dat");
 
 	face.calculate();
 
 	face.test_calc_points(2);
-	for(int i = 0; i < 8; i++)
+	for(int i = 0; i < 9; i++)
 		face.test_func_info(i);
 
 
